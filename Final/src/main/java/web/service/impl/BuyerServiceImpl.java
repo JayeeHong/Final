c6d49881507e64b15466e7692b49bac23949e52a
JayeeHong <hjy48486@naver.com>
2019-04-22 12:59:02 +0900

    초기세팅

---
+++ Final/src/main/java/web/service/impl/BuyerServiceImpl.java
@@ -0,0 +1,10 @@
+package web.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import web.service.face.BuyerService;
+
+@Service
+public class BuyerServiceImpl implements BuyerService {
+
+}
